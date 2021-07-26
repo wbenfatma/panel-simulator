85bea92ba8543cb9f34f7be86f5c98c8aa708ef5
Clement-O <o.clems@hotmail.fr>
2021-07-26 17:51:54 +0200

    Adedd #reverse-proxy and #express server with socket.io
    Signed-off-by: wassim BEN FATMA <wassim.benfatma@kalyzee.com>

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-# panel-simulator
-1) Install npm/node 
-2) Install all the dependencies
-3) --> nodemon app.js - this will start the server 
-4) To change the port open app.js and change it in line number 24. Right now it is 3000
+# Getting started
+## IMPORTANT
+Execute prepare.sh script to install dependencies and create certificates
+Add the addresses to your hosts file (see [hosts](#hosts))<br>
+
+
+## Usage
+Once the certificates are generated and you added the addresses to your hosts, you can:
+- start the panel-simulator --> `docker-compose up` (`-d` to be able to close the terminal and still have the panel running)
+- stop the app --> `docker-compose down`
+<br>
+<br>
+
+# Other
+
+## Certs
+Install `homebrew` and [`mkcert`](https://github.com/FiloSottile/mkcert) (DON'T forget this command: `mkcert -install` ) then execute the script `certs.sh` (Unix: `sh certs.sh`)<br>
+It will create a `certs` folder and files needed to use all `panel.local` urls with HTTPS <br>
+
+## Hosts
+hosts file is usually here:
+- `/etc/hosts` for Mac & Linux
+- `C:\Windows\System32\drivers\etc\hosts` for Windows
+  
+Add those addresses:
+- panel.local
+<br>
--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var server_token = "wassim@benfatma";
-io.on('connection',function(socket){
-    console.log('on user connected '+socket.id);
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const port = 3000
+
+const app = express();
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+const server = require('http').Server(app)
+
+const server_token = "wassim@benfatma";
+
+const socketOptions = {
+  transports: ["websocket"],
+  path: '/socket',
+}
+const io = require('socket.io')(server, socketOptions)
+
+io.on('connection', function(socket) {
+    console.log('on user connected ' + socket.id);
     socket.auth = false;
-    socket.on('login',function(data){
-    var body = data.content;
-    console.log('token recieved is '+body);
-    console.log('connection is authenticated '+socket.id);
-    socket.emit("login",server_token);
+   
+    socket.on('login', function(data) {
+        const body = data.content;
+        console.log('token recieved is '+ body);
+        console.log('connection is authenticated ' + socket.id);
+        socket.emit("login", server_token);
     })
-    socket.on('disconnect',function(){
-        console.log('one user disconnected '+socket.id);
+   
+    socket.on('disconnect', function() {
+        console.log('one user disconnected ' + socket.id);
     })
-    setTimeout(function(){
-        console.log('disconnecting the socket '+socket.id);
+
+    setTimeout(function() {
+        console.log('disconnecting the socket ' + socket.id);
         socket.disconnect();
     },1000);
 })
 
-http.listen(3000,function(){
-    console.log('server listening on port 3000');
+server.listen(port, host, () => {
+  console.log(`Example app listening at https://panel.local`)
 })
