5b2c2427e090c370eff80012bfa26f71dc04eac3
wassim BEN FATMA <wassim.benfatma@kalyzee.com>
2021-09-14 10:07:45 +0200

    Minor refactoring - Created #runCameraCommandsTests allowing to send commands/requests to the camera.
    Signed-off-by: wassim BEN FATMA <wassim.benfatma@kalyzee.com>

--- nodejs_server/index.js
+++ nodejs_server/index.js
@@ -18,18 +18,254 @@ app.get('/',
 
 // used to generate a correlation ID - for test purposes
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
 
 function syncSleep(milliseconds){
- var start = new Date().getTime();
- var end=0;
- while( (end-start) < milliseconds){
-     end = new Date().getTime();
- }
+    var start = new Date().getTime();
+    var end=0;
+    while( (end-start) < milliseconds){
+         end = new Date().getTime();
+    }
+}
+
+function runCameraCommandsTests(socket) {
+
+    // Get system information up request
+    var req = {
+	category: 'system',
+	action: 'get_system_information',
+	correlation_id: uuidv4()
+    };
+    console.log('Get system information JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Move up request
+    req = {
+	category: 'camera',
+	action: 'move',
+	correlation_id: uuidv4(),
+	content: {
+	    direction: 'up'
+	}
+    };
+    console.log('Move JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Move down request
+    req = {
+	category: 'camera',
+	action: 'move',
+	correlation_id: uuidv4(),
+	content: {
+	    direction: 'down'
+	}
+    };
+    console.log('Move JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Move right request
+    req = {
+	category: 'camera',
+	action: 'move',
+	correlation_id: uuidv4(),
+	content: {
+	    direction: 'right'
+	}
+    };
+    console.log('Move JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Move left request
+    req = {
+	category: 'camera',
+	action: 'move',
+	correlation_id: uuidv4(),
+	content: {
+	    direction: 'left'
+	}
+    };
+    console.log('Move JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Stop moving left request
+    req = {
+	category: 'camera',
+	action: 'stop_moving',
+	correlation_id: uuidv4()
+    };
+    console.log('Stop moving JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Zoom add request
+    req = {
+	category: 'camera',
+	action: 'zoom',
+	correlation_id: uuidv4(),
+	content: {
+	    type: 'add'
+	}
+    };
+    console.log('Zoom JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+
+    // Zoom dec request
+    req = {
+	category: 'camera',
+	action: 'zoom',
+	correlation_id: uuidv4(),
+	content: {
+	    type: 'dec'
+	}
+    };
+    console.log('Zoom JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Switch scene request
+    req = {
+	category: 'video',
+	action: 'switch_scene',
+	correlation_id: uuidv4(),
+	content: {
+	    id: 3
+	}
+    };
+    console.log('Switch scene JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Start record request
+    req = {
+	category: 'video',
+	action: 'start_record',
+	correlation_id: uuidv4(),
+	content: {
+	    title: 'test_record_3'
+	}
+    };
+    console.log('Start record  JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Get record session request
+    req = {
+	category: 'video',
+	action: 'get_record_session',
+	correlation_id: uuidv4()
+    };
+    console.log('Get record session JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Stop record request
+    req = {
+	category: 'video',
+	action: 'stop_record',
+	correlation_id: uuidv4()
+    };
+    console.log('Stop record JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Get record session request
+    req = {
+	category: 'video',
+	action: 'get_record_session',
+	correlation_id: uuidv4()
+    };
+    console.log('Get record session JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Create live profile request
+    req = {
+        category: 'publishing',
+	action: 'create_live_profile',
+	correlation_id: uuidv4(),
+	content: {
+	    name: 'wassimbf',
+	    url: "\"rtmp://a.rtmp.youtube.com/live1\"",
+	    username: 'wassim',
+	    password: 'kastKast',
+	    key: 's0j4-w12f-ahvv-7h3b-b3dd'
+	}
+    };
+    console.log('Create live profile JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Read live profile request
+    req = {
+	category: 'publishing',
+	action: 'read_live_profile',
+	correlation_id: uuidv4(),
+	content: {
+	    id: 154
+	}
+    };
+    console.log('Read live profile JSON JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Update live profile request
+    req = {
+	category: 'publishing',
+	action: 'update_live_profile',
+	correlation_id: uuidv4(),
+	content: {
+	    id: 154,
+	    name: 'youtubewassimbfkalyzee',
+	    url: "\"rtmp://a.rtmp.youtube.com/live2\"",
+	    username: 'wassim',
+	    password: 'kastKast',
+	    key: 's0j4-w12f-ahvv-7h3b-b3dv'
+	}
+    };
+    console.log('Update live profile JSON JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Read live profile request
+    req = {
+	category: 'publishing',
+	action: 'read_live_profile',
+	correlation_id: uuidv4(),
+	content: {
+	    id: 154
+	}
+    };
+    console.log('Read live profile JSON JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Enable live profile request
+    req = {
+	category: 'publishing',
+	action: 'enable_live_profile',
+	correlation_id: uuidv4(),
+	content: {
+	    id: 9,
+	    auto: true
+	}
+    };
+    console.log('Enable live profile JSON JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Delete live profile request
+    req = {
+	category: 'publishing',
+	action: 'delete_live_profile',
+	correlation_id: uuidv4(),
+	content: {
+	    id: 147
+	}
+    };
+    console.log('Delete live profile JSON JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
+
+    // Start live session request
+    req = {
+	category: 'video',
+	action: 'start_live',
+	correlation_id: uuidv4()
+    };
+    console.log('Start live JSON request:\n' + JSON.stringify(req));
+    socket.emit('camera', req);
 }
 
 io.on('connection', function(socket) {
@@ -45,300 +281,56 @@ io.on('connection', function(socket) {
 	var auth_token = data.auth_token;
 
 	if (action == 'register') {
-
-		// Retrieve Register request attributes
-		console.log('\nThe camera is registering ...');
-		console.log('Register JSON request:\n' + JSON.stringify(data));
-		console.log('Request category: '+category+'\nRequest action: '+action+'\nCorrelation id: '+correlation_id);
-		console.log('Register request content --> \nCamera id: '+content.camera_id+'\nOrganization Id: '+content.organization_id+'\nPassword: '+content.password+'\nRoom Id: '+content.room_id);
-
-		// Send Register response
-		const resp = {
-		    correlation_id: correlation_id,
-		    type: 'SUCCESS'
-		};
-		console.log('Register JSON response:\n' + JSON.stringify(resp));
-		socket.emit('camera', resp);
-
+	    // Retrieve Register request attributes
+	    console.log('\nThe camera is registering ...');
+	    console.log('Register JSON request:\n' + JSON.stringify(data));
+	    console.log('Request category: '+category+'\nRequest action: '+action+'\nCorrelation id: '+correlation_id);
+	    console.log('Register request content --> \nCamera id: '+content.camera_id+'\nOrganization Id: '+content.organization_id+'\nPassword: '+content.password+'\nRoom Id: '+content.room_id);
+	    // Send Register response
+	    const resp = {
+		correlation_id: correlation_id,
+		type: 'SUCCESS'
+	    };
+	    console.log('Register JSON response:\n' + JSON.stringify(resp));
+	    socket.emit('camera', resp);
 	} else if(action == 'login') {
-
-		// Retrieve Login request attributes
-		console.log('\nThe camera is logging in ...');
-		console.log('Login JSON request:\n' + JSON.stringify(data));
-		console.log('Request category: '+category+'\nRequest action: '+action+'\nCorrelation id: '+correlation_id);
-		console.log('Login request content --> \nCamera id: '+content.camera_id+'\nOrganization Id: '+content.organization_id+'\nPassword: '+content.password);
-
-		// Send Login response
-		var token = crypto.randomBytes(32).toString('hex');
-		const resp = {
-		    correlation_id: correlation_id,
-		    type: 'SUCCESS',
-		    content: {
-			auth_token: token
-		    }
-		};
-		console.log('Login JSON response:\n' + JSON.stringify(resp));
-		socket.emit('camera', resp);
-
-		// Send multiple requests for test purposes
-		syncSleep(10000);
-		console.log('\nStart sending requests to camera...');
-
-		// Get system information up request
-		var req = {
-		    category: 'system',
-		    action: 'get_system_information',
-		    correlation_id: uuidv4()
-		};
-		console.log('Get system information JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Move up request
-		var req = {
-		    category: 'camera',
-		    action: 'move',
-		    correlation_id: uuidv4(),
-		    content: {
-			direction: 'up'
-		    }
-		};
-		console.log('Move JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Move down request
-		req = {
-		    category: 'camera',
-		    action: 'move',
-		    correlation_id: uuidv4(),
-		    content: {
-			direction: 'down'
-		    }
-		};
-		console.log('Move JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Move right request
-		req = {
-		    category: 'camera',
-		    action: 'move',
-		    correlation_id: uuidv4(),
-		    content: {
-			direction: 'right'
-		    }
-		};
-		console.log('Move JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Move left request
-		req = {
-		    category: 'camera',
-		    action: 'move',
-		    correlation_id: uuidv4(),
-		    content: {
-			direction: 'left'
-		    }
-		};
-		console.log('Move JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Stop moving left request
-		req = {
-		    category: 'camera',
-		    action: 'stop_moving',
-		    correlation_id: uuidv4()
-		};
-		console.log('Stop JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Zoom add request
-		req = {
-		    category: 'camera',
-		    action: 'zoom',
-		    correlation_id: uuidv4(),
-		    content: {
-			type: 'add'
-		    }
-		};
-		console.log('Move JSON request:\n' + JSON.stringify(req));
-		socket.emit('message', req);
-
-		console.log('Move JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Zoom dec request
-		req = {
-		    category: 'camera',
-		    action: 'zoom',
-		    correlation_id: uuidv4(),
-		    content: {
-			type: 'dec'
-		    }
-		};
-		console.log('Move JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-
-	       // Switch scene request
-		req = {
-		    category: 'video',
-		    action: 'switch_scene',
-		    correlation_id: uuidv4(),
-		    content: {
-			id: 2
-		    }
-		};
-		console.log('Switch scene JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Start record request
-		req = {
-		    category: 'video',
-		    action: 'start_record',
-		    correlation_id: uuidv4(),
-		    content: {
-			title: 'test_record_3'
-		    }
-		};
-		console.log('Start record  JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Get record session request
-		req = {
-		    category: 'video',
-		    action: 'get_record_session',
-		    correlation_id: uuidv4()
-		};
-		console.log('Get record session JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Stop record request
-		req = {
-		    category: 'video',
-		    action: 'stop_record',
-		    correlation_id: uuidv4()
-		};
-		console.log('Stop record JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Get record session request
-		req = {
-		    category: 'video',
-		    action: 'get_record_session',
-		    correlation_id: uuidv4()
-		};
-		console.log('Get record session JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Create live profile request
-		req = {
-		    category: 'publishing',
-		    action: 'create_live_profile',
-		    correlation_id: uuidv4(),
-		    content: {
-			name: 'wassimbf',
-			url: "\"rtmp://a.rtmp.youtube.com/live1\"",
-			username: 'wassim',
-			password: 'kastKast',
-			key: 's0j4-w12f-ahvv-7h3b-b3dd'
-		    }
-		};
-		console.log('Create live profile JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Read live profile request
-		req = {
-		    category: 'publishing',
-		    action: 'read_live_profile',
-		    correlation_id: uuidv4(),
-		    content: {
-			id: 154
-		    }
-		};
-		console.log('Read live profile JSON JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Update live profile request
-		req = {
-		    category: 'publishing',
-		    action: 'update_live_profile',
-		    correlation_id: uuidv4(),
-		    content: {
-			id: 154,
-			name: 'youtubewassimbfkalyzee',
-			url: "\"rtmp://a.rtmp.youtube.com/live2\"",
-			username: 'wassim',
-			password: 'kastKast',
-			key: 's0j4-w12f-ahvv-7h3b-b3dv'
-		    }
-		};
-		console.log('Update live profile JSON JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		 // Read live profile request
-		req = {
-		    category: 'publishing',
-		    action: 'read_live_profile',
-		    correlation_id: uuidv4(),
-		    content: {
-			id: 154
-		    }
-		};
-		console.log('Read live profile JSON JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Enable live profile request
-		req = {
-		    category: 'publishing',
-		    action: 'enable_live_profile',
-		    correlation_id: uuidv4(),
-		    content: {
-			id: 9,
-			auto: true
-		    }
-		};
-		console.log('Enable live profile JSON JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Delete live profile request
-		req = {
-		    category: 'publishing',
-		    action: 'delete_live_profile',
-		    correlation_id: uuidv4(),
-		    content: {
-			id: 147
-		    }
-		};
-		console.log('Delete live profile JSON JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
-		// Start live session request
-		req = {
-		    category: 'video',
-		    action: 'start_live',
-		    correlation_id: uuidv4()
-		};
-		console.log('Start live JSON request:\n' + JSON.stringify(req));
-		socket.emit('camera', req);
-
+	    // Retrieve Login request attributes
+	    console.log('\nThe camera is logging in ...');
+	    console.log('Login JSON request:\n' + JSON.stringify(data));
+	    console.log('Request category: '+category+'\nRequest action: '+action+'\nCorrelation id: '+correlation_id);
+	    console.log('Login request content --> \nCamera id: '+content.camera_id+'\nOrganization Id: '+content.organization_id+'\nPassword: '+content.password);
+	    // Send Login response
+	    var token = crypto.randomBytes(32).toString('hex');
+	    const resp = {
+		correlation_id: correlation_id,
+		type: 'SUCCESS',
+		content: {
+		    auth_token: token
+		}
+	    };
+	    console.log('Login JSON response:\n' + JSON.stringify(resp));
+	    socket.emit('camera', resp);
+	    // Send multiple requests for test purposes
+	    syncSleep(10000);
+	    console.log('\nStart sending requests to camera...');
+	    // Run Camera commands tests
+	    runCameraCommandsTests(socket);
 	} else if (action == 'logout') {
-
-		// Retrieve Logout request attributes
-		console.log('\nThe camera is logging out ...');
-		console.log('Logout JSON request:\n' + JSON.stringify(data));
-		console.log('Request category: '+category+'\nRequest action: '+action+'\nCorrelation id: '+correlation_id+'\nauth_token: '+auth_token);
-
-		// Send Logout response
-		const resp = {
-		    correlation_id: correlation_id,
-		    type: 'SUCCESS'
-		};
-		console.log('Logout JSON response:\n' + JSON.stringify(resp));
-		socket.emit('camera', resp);
-
+	    // Retrieve Logout request attributes
+	    console.log('\nThe camera is logging out ...');
+	    console.log('Logout JSON request:\n' + JSON.stringify(data));
+	    console.log('Request category: '+category+'\nRequest action: '+action+'\nCorrelation id: '+correlation_id+'\nauth_token: '+auth_token);
+	    // Send Logout response
+	    const resp = {
+		correlation_id: correlation_id,
+		type: 'SUCCESS'
+	    };
+	    console.log('Logout JSON response:\n' + JSON.stringify(resp));
+	    socket.emit('camera', resp);
 	} else {
-		// Retrieve Logout request attributes
-		console.log('\nThe camera sents a response ...');
-		console.log('Camera JSON response:\n' + JSON.stringify(data));
+	    // Retrieve Logout request attributes
+	    console.log('\nThe camera sents a response ...');
+	    console.log('Camera JSON response:\n' + JSON.stringify(data));
 	}
     });
     
